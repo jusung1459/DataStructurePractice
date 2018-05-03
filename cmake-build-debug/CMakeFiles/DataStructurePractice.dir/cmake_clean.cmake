6eef3769d440764450c8bd844369ec8ac9b8f944
JuSung Lee <jusunglee1459@gmail.com>
2018-05-02 19:47:18 -0700

    setup

---
+++ cmake-build-debug/CMakeFiles/DataStructurePractice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructurePractice.dir/main.cpp.o"
+  "DataStructurePractice.pdb"
+  "DataStructurePractice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructurePractice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
