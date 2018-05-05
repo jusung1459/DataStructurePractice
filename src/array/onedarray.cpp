7ce95ada01f47f36e128553a3f9909555b9d0035
JuSung Lee <jusunglee1459@gmail.com>
2018-05-04 19:17:22 -0700

    microandarray

--- src/array/onedarray.cpp
+++ src/array/onedarray.cpp
@@ -1,5 +1,38 @@
 //
 // Created by jusung lee on 2018-05-02.
 //
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 #include "onedarray.h"
+
+int microArray() {
+    int t,n,k;
+    int m;
+    int min;
+    int add;
+
+    //number of test cases
+    cin >> t;
+
+    for(int i=0; i<t; i++) {
+        cin >> n >> k;
+        int a[n];
+        for(int j=0; k>n; j++) {
+            int num;
+            cin >> num;
+            if(num < min) {
+                add = k - num;
+            }
+            a[j] = num;
+        }
+
+        for(int k=0; k<n; k++) {
+            a[k] = a[k] + add;
+        }
+    }
+    return 0;
+
+}
