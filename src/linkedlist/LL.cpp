946bd01045cae3687409d7522e6e7b2534ac718c
JuSung Lee <jusunglee1459@gmail.com>
2018-05-26 23:28:50 -0700

    update

--- src/linkedlist/LL.cpp
+++ src/linkedlist/LL.cpp
@@ -1,2 +1,33 @@
 #include "LL.h"
 
+class Node {
+    int val;
+}
+
+class LinkedList {
+    public:
+        //constructor
+        LinkedList() {
+        
+        }
+        //destructor
+        ~LinkedList() {
+
+        }
+        //is list empty?
+        bool empty() const {
+            
+        }
+        //get front element
+        const int front() const {
+
+        }
+        //add element to front
+        void addFront(const int num) {
+
+        }
+        //remove front element from list
+        void remove() {
+            
+        }
+}
