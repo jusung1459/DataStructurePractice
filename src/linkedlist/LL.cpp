1f27587492a5aa13dc3a0f8477a34e9bb85fbb9e
JuSung Lee <jusunglee1459@gmail.com>
2018-05-27 19:57:16 -0700

    ll

--- src/linkedlist/LL.cpp
+++ src/linkedlist/LL.cpp
@@ -1,33 +1,59 @@
-#include "LL.h"
-
 class Node {
-    int val;
-}
+    private:
+        int data;
+        Node* next;
+
+        friend class LinkedList;
+};
 
 class LinkedList {
     public:
         //constructor
-        LinkedList() {
-        
-        }
+        LinkedList();
         //destructor
-        ~LinkedList() {
-
-        }
+        ~LinkedList();
         //is list empty?
-        bool empty() const {
-            
-        }
+        bool empty() const;
         //get front element
-        const int front() const {
-
-        }
+        const int front() const;
         //add element to front
-        void addFront(const int num) {
-
-        }
+        void addFront(const int num);
         //remove front element from list
-        void remove() {
-            
-        }
+        void remove();
+    private:
+        Node* head;
+};
+
+ //constructor
+LinkedList::LinkedList() 
+    : head(nullptr){}
+
+//destructor
+LinkedList::~LinkedList() {
+    while(!empty()){
+        remove();
+    }
+}
+//is list empty?
+bool LinkedList::empty() const {
+    return head == nullptr;
+ }
+//get front element
+const int LinkedList::front() const {
+    return head->data;
+}
+//add element to front
+void LinkedList::addFront(const int num) {
+    Node* node = new Node;
+    node->data = num;
+    node->next = head;
+    head = node;
+}
+//remove front element from list
+void LinkedList::remove() {
+    if(!empty()) {
+        Node* oldhead = head;
+        head = oldhead->next;
+        delete oldhead;
+    }
 }
