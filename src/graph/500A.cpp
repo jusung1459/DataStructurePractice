abab9cf85fb6c58a4da7765471e5f0bb32f08087
JuSung Lee <jusunglee1459@gmail.com>
2018-10-01 16:11:06 -0700

    link

--- src/graph/500A.cpp
+++ src/graph/500A.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
+//http://codeforces.com/problemset/problem/500/A
 int main()
 {
-    int x,y;
+    int x, y;
     cin >> x >> y;
     int board[y];
 
-    for (int i=0; i<x; i++) {
+    for (int i = 0; i < x; i++)
+    {
         cin >> board[i];
     }
-    
-    for(int j=0; j<x; j++) {
+
+    for (int j = 0; j < x; j++)
+    {
         cout << board[j];
     }
 
