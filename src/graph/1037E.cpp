ea42b6499a84f09769cbe99ef19c8682371738af
JuSung Lee <jusunglee1459@gmail.com>
2018-10-07 12:41:35 -0700

    fisnished 500A added 1037E

---
+++ src/graph/1037E.cpp
@@ -0,0 +1,9 @@
+//http://codeforces.com/problemset/problem/1037/E
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
--- src/graph/500A.cpp
+++ src/graph/500A.cpp
@@ -7,16 +7,38 @@ int main()
     int x, y;
     cin >> x >> y;
     int board[5000];
-    for (int i = 0; i < x; i++)
+    for (int i = 1; i < x; i++)
     {
         cin >> board[i];
     }
+
+    bool cango = false;
 
     for (int i = 1; i < x;)
     {
         if (i + board[i] == y)
         {
+            cango = true;
+            i = board[i];
+            break;
         }
+        else if (i + board[i] > y)
+        {
+            break;
+        }
+        else
+        {
+            cango = false;
+            i = i + board[i];
+        }
+    }
+    if (cango)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
     }
     return 0;
 }
