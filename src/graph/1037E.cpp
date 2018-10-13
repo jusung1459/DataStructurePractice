0cb19ee75d58d58762a2713e999a58bedde718d0
JuSung Lee <jusunglee1459@gmail.com>
2018-10-12 17:07:49 -0700

    1037E

--- src/graph/1037E.cpp
+++ src/graph/1037E.cpp
@@ -7,7 +7,16 @@ int main()
 {
     int n, m, k;
     cin >> n >> m >> k;
-    int graph[n][m];
+    int graph[n][n];
+
+    //none are friends
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; i < n; i++)
+        {
+            graph[i][k] = 0;
+        }
+    }
 
     for (int i = 1; i < n; i++)
     {
