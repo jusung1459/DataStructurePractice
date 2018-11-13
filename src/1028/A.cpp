6eca763e10f42228a4560ca061fad3cc85d5da28
JuSung Lee <jusunglee1459@gmail.com>
2018-11-12 20:05:50 -0800

    1028A

--- src/1028/A.cpp
+++ src/1028/A.cpp
@@ -1,1 +1,16 @@
 //http://codeforces.com/problemset/problem/1028/A
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n, m;
+    int table[n][m];
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j>m; j++) {
+            
+        }
+    }
+    return 0;
+}
