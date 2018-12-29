73c8ef2e2ac6297e33ce06c44f25a5173707e898
JuSung Lee <jusunglee1459@gmail.com>
2018-12-28 20:05:59 -0800

    in progress
    work

--- src/1028/A.cpp
+++ src/1028/A.cpp
@@ -5,11 +5,16 @@ using namespace std;
 int main() {
     int n,m;
     cin >> n, m;
-    int table[n][m];
+    char table[115][115];
 
     for(int i=0; i<n; i++) {
         for(int j=0; j>m; j++) {
-            
+            char color;
+            cin >> color;
+            table[i][j] = color;
+            if(color == "B") {
+                
+            }
         }
     }
     return 0;
